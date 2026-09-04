076fa80f2c687af3019a8237c569b841f1ea8ed9
insuk26 <insook99@gmail.com>
2026-09-04 17:02:21 +0900

    0904

---
+++ js/swHero.js
@@ -0,0 +1,10 @@
+const swHero = new Swiper(".sw-hero", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
