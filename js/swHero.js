a186173d2478d947234c952e625e5206b1c746db
insuk26 <insook99@gmail.com>
2026-09-07 16:41:52 +0900

    스와이퍼 구현중, slideChangeTrandsitionStart 매서드 구현

--- js/swHero.js
+++ js/swHero.js
@@ -1,10 +1,33 @@
+const mainVisualBg = document.querySelector(
+  ".hero .main-visual .main-visual-bg",
+);
+// console.log(mainVisualBg);
+
 const swHero = new Swiper(".sw-hero", {
+  speed: 1000,
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".hero-next-btn",
+    prevEl: ".hero-prev-btn",
+  },
+
+  on: {
+    slideChangeTransitionStart: function (swiper) {
+      // const activeSilde = swiper.activeIndex + 1;
+      // console.log(activeSilde);
+      // mainVisualBg.style.backgroundImage = `url(assets/images/slide_${activeSilde}.png)`;
+      const activeSilde = swiper.slide[swiper.activeIndex];
+      // console.log(activeSlide);
+      const activeSildeImg = activeSilde.querySelector(".slide-image img");
+      // console.log(activeSlideImg.src);
+      mainVisualBg.style.backgroundImage = `url(${actibeSlideImg.src})`;
+    },
   },
 });
