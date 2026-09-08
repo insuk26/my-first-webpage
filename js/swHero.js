4c581954cfcbd22fb79bfeb79e4c4a94958e903a
insuk26 <insook99@gmail.com>
2026-09-08 16:50:01 +0900

    히어로 슬라이드 스와이퍼 내비게이션 버튼 비활성화, 자동재생 버튼 구현

--- js/swHero.js
+++ js/swHero.js
@@ -2,13 +2,30 @@ const mainVisualBg = document.querySelector(
   ".hero .main-visual .main-visual-bg",
 );
 // console.log(mainVisualBg);
+const heroBtns = document.querySelectorAll(".visual-inner .sw-hero-btn");
+// console.log(heroBtns);
+const playStopBtn = document.querySelector(
+  ".swHero .pagination .play-stop-btn",
+);
 
+// 히어로 슬라이드 버튼 호버 인터랙션
+heroBtns.forEach((btn) => {
+  btn.addEventListener("mouseenter", () => {
+    btn.querySelector(".off").style.opacity = 1;
+  });
+
+  btn.addEventListener("mouseout", () => {
+    btn.querySelector(".off").style.opacity = 0;
+  });
+});
+
+// 스와이퍼 설정
 const swHero = new Swiper(".sw-hero", {
   speed: 1000,
-  // autoplay: {
-  //   delay: 5000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -25,9 +42,22 @@ const swHero = new Swiper(".sw-hero", {
       // mainVisualBg.style.backgroundImage = `url(assets/images/slide_${activeSilde}.png)`;
       const activeSlide = swiper.slides[swiper.activeIndex];
       // console.log(activeSlide);
-      const activeSlideImg = activeSlide.querySelector(".slide-image img");
+      const activeSlideImgUrl =
+        activeSlide.querySelector(".slide-image img").src;
       // console.log(activeSlideImg.src);
-      mainVisualBg.style.backgroundImage = `url(${activeSlideImg.src})`;
+      mainVisualBg.style.backgroundImage = `url(${activeSlideImgUrl})`;
     },
   },
+});
+
+// 재생, 정지 버튼
+playStopBtn.addEventListener("clik", () => {
+  const isRunning = swHero.autoplay.running;
+  // console.log(isRunning);
+
+  if (isRunning) {
+    swHero.autoplay.stop();
+  } else {
+    swHero.autoplay;
+  }
 });
