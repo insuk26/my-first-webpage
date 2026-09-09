261bdbd231551daa35ffded13ceaf76bce4c0fa7
insuk26 <insook99@gmail.com>
2026-09-09 16:49:47 +0900

    히어로 슬라이드 페이지네이션 스타일링

--- js/swHero.js
+++ js/swHero.js
@@ -5,15 +5,15 @@ const mainVisualBg = document.querySelector(
 const heroBtns = document.querySelectorAll(".visual-inner .sw-hero-btn");
 // console.log(heroBtns);
 const playStopBtn = document.querySelector(
-  ".swHero .pagination .play-stop-btn",
+  ".sw-hero .pagination .play-stop-btn",
 );
+// console.log(playStopBtn);
 
 // 히어로 슬라이드 버튼 호버 인터랙션
 heroBtns.forEach((btn) => {
   btn.addEventListener("mouseenter", () => {
     btn.querySelector(".off").style.opacity = 1;
   });
-
   btn.addEventListener("mouseout", () => {
     btn.querySelector(".off").style.opacity = 0;
   });
@@ -51,13 +51,21 @@ const swHero = new Swiper(".sw-hero", {
 });
 
 // 재생, 정지 버튼
-playStopBtn.addEventListener("clik", () => {
+playStopBtn.addEventListener("click", () => {
+  // console.log("슬라이드 자동재생 정지");
+
   const isRunning = swHero.autoplay.running;
   // console.log(isRunning);
 
   if (isRunning) {
     swHero.autoplay.stop();
+    console.log("자동재생 정지");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_play.png)`;
+    playStopBtn.textContent = "재생";
   } else {
-    swHero.autoplay;
+    swHero.autoplay.start();
+    console.log("자동재생 시작");
+    playStopBtn.style.backgroundImage = `url(assets/icons/slide_stop.png)`;
+    playStopBtn.textContent = "정지";
   }
 });
