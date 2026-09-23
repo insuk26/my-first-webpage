7b08f4acb4fca46ca44f687070b394cfdbf0050c
insuk26 <insook99@gmail.com>
2026-09-23 17:20:06 +0900

    구현 완료

--- js/swBanner.js
+++ js/swBanner.js
@@ -1,18 +1,14 @@
 // 1. 배너 영역에서 사용할 HTML 요소 선택
 // 배너 영역 전체
 const bannerSection = document.querySelector(".banner");
-// Swiper를 저굥ㅇ할 슬라이더 컨테이너
+// Swiper를 적용할 슬라이더 컨테이너
 const bannerContainer = bannerSection.querySelector(".sw-banner");
-// console.log(bannerSection, bannerContainer);
-
-// JSON 데이터를 바탕으로 마든 swiper-slide 요소들을 넣을 공간
+// JSON 데이터를 바탕으로 만든 swiper-slide 요소들을 넣을 공간
 const bannerWrapper = bannerContainer.querySelector(".swiper-wrapper");
-
 // 이전, 다음, 페이지 번호, 정지 버튼을 감싸는 영역
 const bannerControls = bannerSection.querySelector(".pagination");
-
 // 재생, 정지 버튼
-const bannerplayStopBtn = bannerSection.querySelector(".play-stop-btn");
+const bannerPlayStopBtn = bannerSection.querySelector(".play-stop-btn");
 
 // 2. JSON 파일에서 배너 데이터 불러오기
 async function fetchBanners() {
@@ -20,16 +16,44 @@ async function fetchBanners() {
 
   if (!response.ok) {
     // 이 오류는 loadBanners() 의 catch에서 처리
-    throw new Error(`배너 요청 실패: $(response.status)`);
+    throw new Error(`배너 요청 실패: ${response.status}`);
   }
+
   // 응답 본문을 읽고 JSON을 javascript 데이터로 변환
   const banners = await response.json();
 
   return banners;
 }
-// 3. map()과 템플릿 문자열 배너 HTML 생성
+
+// 3. map()과 템플릿 문자열로 배너 HTML 생성
 function renderBanners(banners) {
-  const bannerHTML = banners.map(banner);
+  const bannerHTML = banners
+    .map((banner) => {
+      return `
+        <div class="swiper-slide">
+          <a href="#">
+            <img
+              src="${banner.bannerImg}"
+              alt="${banner.bannerTitle}"
+            />
+            <div class="text-wrap">
+              <span class="desc">${banner.desc}</span>
+              <strong>${banner.bannerTitle}</strong>
+              <div class="link-btn">
+                <span>${banner.linkLabel}</span>
+                <img
+                  src="assets/icons/icon_link.png"
+                  alt="바로가기 아이콘"
+                />
+              </div>
+            </div>
+          </a>
+        </div>
+        `;
+    })
+    .join("");
+
+  bannerWrapper.innerHTML = bannerHTML;
 }
 
 // 4. 생성된 배너에 Swiper 적용
@@ -60,15 +84,35 @@ function initBannerSwiper() {
 
     if (isRunning) {
       swBanner.autoplay.stop();
-      console.log("자동재생 정지");
+      //   console.log("자동재생 정지");
       bannerPlayStopBtn.style.backgroundImage = `url(assets/icons/slide_play_bk.png)`;
       bannerPlayStopBtn.textContent = "재생";
     } else {
       swBanner.autoplay.start();
-      console.log("자동재생 시작");
+      //   console.log("자동재생 시작");
       bannerPlayStopBtn.style.backgroundImage = `url(assets/icons/slide_stop_bk.png)`;
       bannerPlayStopBtn.textContent = "정지";
     }
   });
 }
+
 // 5. 전체 작업을 순서대로 실행
+async function loadBanners() {
+  try {
+    const banners = await fetchBanners();
+
+    // 배열은 정상적으로 받았지만 항목이 없는 경우를 처리
+    if (banners.length === 0) {
+      return;
+    }
+
+    // 배너 데이터를 HTML로 만들어서 화면에 넣음
+    renderBanners(banners);
+    // Swiper 초기화
+    initBannerSwiper();
+  } catch (error) {
+    console.log(`배너 로딩 오류: ${error}`);
+  }
+}
+
+loadBanners();
